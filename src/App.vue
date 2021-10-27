1503279ab07d6acd9d1c4f22cef3d097c0577c85
Anders Jensen <Anders@Katrines-MBP-2.lan>
2021-10-27 21:59:39 +0200

    added base for grid, with a bit of styling

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,68 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
+  <grid
+        :entries="gridData"
+        :columns="gridColumns"
       >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <HelloWorld/>
-    </v-main>
-  </v-app>
+      </grid>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import Grid from './components/Grid';
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    Grid,
+  },
+  props: {
   },
 
   data: () => ({
-    //
+    gridColumns: ["name", "brand", "rating", "note", "origin"],
+    gridData: [
+      { name: "Stout Cask", brand: "Teeling", rating: "8/10", note: "Try the Rum Cask next", origin: "Ireland" },
+      { name: "16", brand: "Lagavulin", rating: "6/10", note: "Smokey boy", origin: "Scotland" },
+      { name: "Blended Malt", brand: "Monkey Shoulder", rating: "7/10", note: "", origin: "Scotland" }
+    ]
   }),
 };
 </script>
+
+<style lang="scss">
+$main-color: #1D1B1B;
+$secondary-color: #EC4D37;
+
+body {
+  font-family: Helvetica Neue, Arial, sans-serif;
+  font-size: 14px;
+  color: $secondary-color;
+  background-color: $main-color;
+}
+
+table {
+  border: 2px solid $secondary-color;
+  border-radius: 3px;
+  background-color: $main-color;
+  width: 100%;
+  border-collapse: collapse;
+}
+
+th {
+  background-color: $secondary-color;
+  color: $main-color;
+}
+
+td {
+  background-color: $main-color;
+  border: 1px solid $secondary-color;
+}
+
+th,
+td {
+  min-width: 120px;
+  padding: 10px 20px;
+}
+
+</style>
+
---
+++ src/components/Grid.vue
@@ -0,0 +1,46 @@
+<template>
+    <v-container>
+        <table>
+            <thead>
+                <tr>
+                    <th 
+                        v-for="key in columns"
+                        :key="key.name">
+                        {{ key | capitalize }}
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr 
+                    v-for="entry in allEntries"
+                    :key="entry.name">
+                <td 
+                    v-for="key in columns"
+                    :key="key.name">
+                    {{entry[key]}}
+                </td>
+          </tr>
+            </tbody>
+        </table>
+    </v-container>
+</template>
+
+<script>
+  export default {
+    props: {
+        columns: Array,
+        entries: Array,
+    },
+    computed: {
+          allEntries: function() {
+            let entries = this.entries;
+            return entries;
+          }
+    },
+    filters: {
+          capitalize: function(str) {
+            return str.charAt(0).toUpperCase() + str.slice(1);
+          }
+        }
+  }
+</script>
